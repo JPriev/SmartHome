4dc118d91b5d38d19635b6cb63d7389ffd859920
JPriev <prievelis.jonas@gmail.com>
2020-04-06 16:12:58 +0300

    login update

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,61 @@
   
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
 
 function Login() {
     return <Container>
         <Row>
-            <Col xs={12}>
-                <p>Login</p>
-            </Col>
+            <form> {/* neveikia xs={12} del form */}
+                <Col xs={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroup-sizing-default">Username</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            type="text"
+                            placeholder="Enter your username"
+                            aria-label="Default"
+                            aria-describedby="inputGroup-sizing-default"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col xs={12}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroup-sizing-default">Password</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl
+                            type="password"
+                            placeholder="Enter your password"
+                            aria-label="Default"
+                            aria-describedby="inputGroup-sizing-default"
+                        />
+                    </InputGroup>
+                </Col>
+                <Col xs={12}>
+                    <Button color="primary">Login</Button>
+                </Col>
+            </form>
         </Row>
     </Container>
+
+{/* <form onSubmit={this.login}>
+<FormGroup controlId="email" validationState={ formSubmitted ? (errors.email ? 'error' : 'success') : null }>
+    <ControlLabel>Email</ControlLabel>
+    <FormControl type="text" name="email" placeholder="Enter your email" onChange={this.handleInputChange} />
+{ errors.email &&
+    <HelpBlock>{errors.email}</HelpBlock>
+}
+</FormGroup>
+<FormGroup controlId="password" validationState={ formSubmitted ? (errors.password ? 'error' : 'success') : null }>
+    <ControlLabel>Password</ControlLabel>
+    <FormControl type="password" name="password" placeholder="Enter your password" onChange={this.handleInputChange} />
+{ errors.password &&
+    <HelpBlock>{errors.password}</HelpBlock>
+}
+</FormGroup>
+<Button type="submit" bsStyle="primary">Sign-In</Button>
+</form> */}
             // <div class="container" style="margin-top:20px;">
             
             //     <form>
