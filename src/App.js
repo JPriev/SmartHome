19eb5f142a5712ee113487d17a2526681c7ca233
JPriev <prievelis.jonas@gmail.com>
2020-04-06 19:55:05 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
   
 import React, { useState, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 import Login from './Login'
 
 export default () => {
   return <Router>
     <Switch>
-      <Route path="/" component = {Login}>
+      <Route path="/" exact component = {Login}>
       </Route>
     </Switch>
   </Router>
