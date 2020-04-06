ff6001b49c935dd3686c1a7b66e0d858a219298f
JPriev <prievelis.jonas@gmail.com>
2020-04-06 14:23:19 +0300

    update

---
+++ src/Login.js
@@ -0,0 +1,40 @@
+  
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Login() {
+    return <Container>
+        <Row>
+            <Col xs={12}>
+                <p>Login</p>
+            </Col>
+        </Row>
+    </Container>
+            // <div class="container" style="margin-top:20px;">
+            
+            //     <form>
+            //         <div class="form-group row">
+            //             <label class="col-sm-3 col-form-label" for="email">Email Address</label>
+            //             <div class="col-sm-9">
+            //                 <input type="text" class="form-control" id="email">
+            //         </div>
+                    
+            //         <div class="form-group row">
+            //             <label class="col-sm-3 col-form-label" for="password">Password</label>
+            //             <div class="col-sm-9">
+            //                 <input type="password" class="form-control" id="password">
+            //             </div>
+            //         </div>
+                    
+            //         <div class="row">
+            //             <div class="col-sm-9 offset-sm-3">
+            //                 <button type="submit" class="btn btn-primary">Login Now</button>
+            //             </div>
+            //         </div>
+                    
+            //     </form>
+            
+            // </div>
+}
+
+export default Login;
